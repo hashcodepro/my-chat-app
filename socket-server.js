d7919fc7964479cf36edc293c62b4aa7772d0d96
Subbiah <Yudhaamanyu.Subbiah@travelex.com>
2019-03-23 12:42:21 +0530

    my first commit

---
+++ socket-server.js
@@ -0,0 +1,27 @@
+var express = require("express");
+var app = express();
+var http = require("http");
+var server = http.createServer(app);
+var io = require("socket.io")(server);
+
+var port = process.env.PORT || 3000;
+
+io.on("connection", client => {
+    console.log("Client Connected.\nClient Id : ", client.id);
+    client.emit("acknowledge", { message: "You are connected" })
+    client.on("msgToServer", (chatterName, msg) => {
+        console.log(chatterName + " says : " + msg);
+        client.broadcast.emit("serverMsg", chatterName, msg);
+        client.emit("serverMsg", "Me", msg);
+    })
+})
+
+
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/socket-client.html");
+})
+
+server.listen(3000, () => {
+    console.log("Socket server running on Port " + port);
+})
